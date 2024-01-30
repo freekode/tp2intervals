2e83682dcf7185148611bb7e018a0297b3250841
Evgeny <iam@freekode.org>
2024-01-30 13:38:53 +0100

    map any type of workouts
    fix #3

--- boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/intervalsicu/IntervalsApiClient.kt
+++ boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/intervalsicu/IntervalsApiClient.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody
     value = "IntervalsApiClient",
     url = "\${intervals.api-url}",
     dismiss404 = true,
+    primary = false,
     configuration = [IntervalsApiClientConfig::class]
 )
 interface IntervalsApiClient {
