1f580c54f1f0a714a0087e576000f636558268dd
Evgeny <iam@freekode.org>
2024-02-03 21:40:07 +0100

    upd gh link padding, use hash routing

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -8,7 +8,7 @@ import { httpErrorInterceptor } from "infrastructure/http-error.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(withInterceptors([httpErrorInterceptor]))
   ]
