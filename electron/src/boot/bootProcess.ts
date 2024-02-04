c466519605fd715ae45c71ed7a6096de0063af51
Evgeny <iam@freekode.org>
2024-02-04 19:52:36 +0100

    prepare jar in electron

---
+++ electron/src/boot/bootProcess.ts
@@ -0,0 +1,57 @@
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import path from 'path';
+import { app } from 'electron';
+import log from 'electron-log';
+import { isWindows } from "./platform";
+
+export class BootProcess {
+    private readonly bootPath = path.join(process.resourcesPath, 'tp2intervals-static.jar');
+    private readonly bootDbPath = path.join(app.getPath('userData'), 'tp2intervals.sqlite');
+    private readonly jdkPath = path.join(process.resourcesPath, 'jdk', 'bin', isWindows ? 'java.exe' : 'java');
+    private readonly port: number;
+    private readonly address: string;
+
+    private childProcess?: ChildProcessWithoutNullStreams = undefined;
+
+    constructor() {
+        this.port = Math.floor(Math.random() * 10000) + 10000;
+        this.address = `http://localhost:${this.port}`;
+    }
+
+    async start(): Promise<void> {
+        if (this.childProcess) {
+            log.warn('Packaged boot is already started');
+            return;
+        }
+
+        const env = {
+            SERVER_PORT: String(this.port),
+            SPRING_DATASOURCE_URL: `jdbc:sqlite:${this.bootDbPath}`,
+        };
+
+        log.info('Running boot from jar...');
+        this.childProcess = spawn(this.jdkPath, ['-jar', this.bootPath], {
+            env: {...process.env, ...env},
+        });
+        this.initProcessEvents();
+    }
+
+    private initProcessEvents() {
+        if (!this.childProcess) {
+            return;
+        }
+
+        this.childProcess.stdout.on('data', (data) => {
+            log.info(`boot: ${data}`);
+        });
+
+        this.childProcess.stderr.on('data', (data) => {
+            log.error(`boot: ${data}`);
+        });
+
+        process.on('exit', () => {
+            this.childProcess?.kill();
+        });
+    }
+
+}
--- electron/src/index.ts
+++ electron/src/index.ts
@@ -1,8 +1,7 @@
 import { app, BrowserWindow } from 'electron';
-// import installExtension from 'electron-devtools-installer';
+import installExtension from 'electron-devtools-installer';
 import path from 'path';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
@@ -10,7 +9,6 @@ if (require('electron-squirrel-startup')) {
 let mainWindow: BrowserWindow | null;
 
 const createWindow = (): void => {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     height: 800,
     width: 1280,
@@ -19,7 +17,6 @@ const createWindow = (): void => {
     },
   });
 
-  // This should be the name of your angular project instead of `my-app`
   const startURL = app.isPackaged
     ? `file://${path.join(__dirname, 'browser', 'index.html')}`
     : `http://localhost:4200`;
@@ -27,24 +24,18 @@ const createWindow = (): void => {
   mainWindow.loadURL(startURL);
 };
 
-app.whenReady().then(() => {
-  // Install Angular DevTools
-  // https://angular.io/guide/devtools
-  // installExtension('ienfalfjdbdpebioblfackkekamfmbnh')
-  //   .then((name) => console.log(`Added Extension:  ${name}`))
-  //   .catch((err) => console.log('An error occurred: ', err));
-});
+const installDevTools = () => {
+  installExtension('ienfalfjdbdpebioblfackkekamfmbnh')
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-  createWindow();
-});
+app.whenReady()
+  .then(async () => {
+    createWindow();
+  })
+  .catch(console.log);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -52,12 +43,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
