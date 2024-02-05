225b68e90d31542f142c8f32208a67a8b86b7107
Evgeny <iam@freekode.org>
2024-02-05 16:16:28 +0100

    electron is ok

--- electron/forge.config.ts
+++ electron/forge.config.ts
@@ -2,26 +2,27 @@ import type { ForgeConfig } from '@electron-forge/shared-types';
 import MakerSquirrel from '@electron-forge/maker-squirrel';
 import MakerZIP from '@electron-forge/maker-zip';
 import MakerDeb from '@electron-forge/maker-deb';
-import MakerRpm from '@electron-forge/maker-rpm';
+import MakerDMG from '@electron-forge/maker-dmg';
 import AutoUnpackNativesPlugin from '@electron-forge/plugin-auto-unpack-natives';
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     extraResource: [
-        '../boot/build/libs/tp2intervals-static.jar',
-        '../devops/jdktool/jdks/${os}/${arch}'
-    ]
+      '../boot/build/libs/tp2intervals-static.jar',
+      '../devops/jdktool/jdks/linux/x64'
+    ],
+
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
-    // new MakerRpm({}),
-    new MakerDeb({}),
+    new MakerZIP({}),
+    new MakerDMG({}, ['darwin']),
+    new MakerDeb({}, ['linux']),
   ],
-  plugins: [new AutoUnpackNativesPlugin({})],
   publishers: [],
+  plugins: [new AutoUnpackNativesPlugin({})],
 };
 
 export default config;
