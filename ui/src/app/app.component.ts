a6d826c9b3ea2217eaf272a0d743216c44fef961
Evgeny <iam@freekode.org>
2024-03-21 13:29:17 +0100

    updated menu

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,25 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { UpdateService } from "infrastructure/update.service";
 import { EnvironmentService } from "infrastructure/environment.service";
+import { MatOptionModule } from "@angular/material/core";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatCardModule, MatIconModule, MatToolbarModule, RouterLink, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterLink,
+    MatButtonModule,
+    MatOptionModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   appVersion: string
 
+  menuButtons = [
+    {name: 'Home', url: '/home'},
+    {name: 'TrainingPeaks', url: '/training-peaks'},
+    {name: 'TrainerRoad', url: '/trainer-road'},
+    {name: 'Configuration', url: '/config'},
+  ]
+
   constructor(
+    protected router: Router,
     private updateService: UpdateService,
     private environmentService: EnvironmentService
   ) {
