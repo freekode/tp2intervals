aba8446a7099d53b9ec425877db3059d8581f537
Evgeny <iam@freekode.org>
2024-03-30 20:39:56 +0100

    Add cache for config validation

--- boot/src/main/kotlin/org/freekode/tp2intervals/domain/Platform.kt
+++ boot/src/main/kotlin/org/freekode/tp2intervals/domain/Platform.kt
@@ -3,7 +3,7 @@ package org.freekode.tp2intervals.domain
 enum class Platform(
     val title: String,
     val key: String,
-    ) {
+) {
     INTERVALS("Intervals.icu", "intervals"),
     TRAINING_PEAKS("TrainingPeaks", "training-peaks"),
     TRAINER_ROAD("TrainerRoad", "trainer-road"),
