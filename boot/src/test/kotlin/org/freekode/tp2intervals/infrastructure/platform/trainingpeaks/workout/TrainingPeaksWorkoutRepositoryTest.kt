5989cd0e9a3e7e5021073125094d873a728b2d4f
Evgeny <iam@freekode.org>
2024-04-08 16:55:39 +0200

    tprepo

--- boot/src/test/kotlin/org/freekode/tp2intervals/infrastructure/platform/trainingpeaks/workout/TrainingPeaksWorkoutRepositoryTest.kt
+++ boot/src/test/kotlin/org/freekode/tp2intervals/infrastructure/platform/trainingpeaks/workout/TrainingPeaksWorkoutRepositoryTest.kt
@@ -5,7 +5,6 @@ import java.time.LocalDate
 import org.freekode.tp2intervals.domain.ExternalData
 import org.freekode.tp2intervals.domain.librarycontainer.LibraryContainer
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -14,9 +13,8 @@ class TrainingPeaksWorkoutRepositoryTest : SpringITConfig() {
     lateinit var trainingPeaksWorkoutRepository: TrainingPeaksWorkoutRepository
 
     @Test
-    @Disabled
     fun test() {
-        val container = LibraryContainer("lib", LocalDate.now(), false, 0, ExternalData.empty())
+        val container = LibraryContainer("lib", LocalDate.now(), false, 0, ExternalData.empty().withTrainingPeaks("tp-id"))
         val workouts = trainingPeaksWorkoutRepository.getWorkoutsFromLibrary(container)
 
         assertTrue(workouts.isNotEmpty())
