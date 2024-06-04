2c099d5754a93af3b0f350227070b7637c3652c4
Evgeny <iam@freekode.org>
2024-06-04 10:56:43 +0200

    upd tests

--- boot/src/test/kotlin/org/freekode/tp2intervals/infrastructure/platform/trainingpeaks/workout/TrainingPeaksWorkoutRepositoryTest.kt
+++ boot/src/test/kotlin/org/freekode/tp2intervals/infrastructure/platform/trainingpeaks/workout/TrainingPeaksWorkoutRepositoryTest.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 import org.freekode.tp2intervals.domain.ExternalData
 import org.freekode.tp2intervals.domain.librarycontainer.LibraryContainer
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -13,8 +13,15 @@ class TrainingPeaksWorkoutRepositoryTest : SpringITConfig() {
     @Autowired
     lateinit var trainingPeaksWorkoutRepository: TrainingPeaksWorkoutRepository
 
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            Thread.sleep(1000)
+        }
+    }
+
     @Test
-    @Disabled
     fun `should parse workout library`() {
         val container =
             LibraryContainer("lib", LocalDate.now(), false, 0, ExternalData.empty().withTrainingPeaks("tp-id"))
@@ -24,7 +31,6 @@ class TrainingPeaksWorkoutRepositoryTest : SpringITConfig() {
     }
 
     @Test
-    @Disabled
     fun `should parse calendar workout`() {
         val workouts = trainingPeaksWorkoutRepository.getWorkoutsFromCalendar(LocalDate.now(), LocalDate.now())
 
