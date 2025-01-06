74bb611d84b2b22b0530e0c57b05d51ea8e154ec
Evgeny <iam@freekode.org>
2025-01-06 20:00:43 +0100

    deprecate strava

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,10 +1,9 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from "app/home/home.component";
-import { ConfigurationComponent } from "app/configuration/configuration.component";
-import { canActivateHome } from "app/home/can-activate-home";
-import { TrainingPeaksComponent } from "app/training-peaks/training-peaks.component";
-import { TrainerRoadComponent } from "app/trainer-road/trainer-road.component";
-import {StravaComponent} from "app/strava/strava.component";
+import {Routes} from '@angular/router';
+import {HomeComponent} from "app/home/home.component";
+import {ConfigurationComponent} from "app/configuration/configuration.component";
+import {canActivateHome} from "app/home/can-activate-home";
+import {TrainingPeaksComponent} from "app/training-peaks/training-peaks.component";
+import {TrainerRoadComponent} from "app/trainer-road/trainer-road.component";
 
 export const routes: Routes = [
   {
@@ -20,11 +19,6 @@ export const routes: Routes = [
   {
     path: 'trainer-road',
     component: TrainerRoadComponent,
-    canActivate: [canActivateHome]
-  },
-  {
-    path: 'strava',
-    component: StravaComponent,
     canActivate: [canActivateHome]
   },
   {
