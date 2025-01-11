2a14aefafc01416fbb75f9b385e20983d8ec94a3
Evgeny <iam@freekode.org>
2025-01-11 18:07:06 +0100

    debug mode

--- boot/src/main/kotlin/org/freekode/tp2intervals/domain/config/DebugModeService.kt
+++ boot/src/main/kotlin/org/freekode/tp2intervals/domain/config/DebugModeService.kt
@@ -5,24 +5,34 @@ import org.springframework.boot.logging.LogLevel
 import org.springframework.boot.logging.LoggingSystem
 import org.springframework.stereotype.Component
 
+
 @Component
-class LogLevelService(
+class DebugModeService(
     private val appConfigurationRepository: AppConfigurationRepository
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass)
+    private val generalDebugModeKey = "general.debug-mode"
 
     init {
-        initLogLevel()
+        initDebugMode()
     }
 
-    fun setLogLevel(logLevel: LogLevel) {
+    fun handleDebugMode(configMap: Map<String, String?>) {
+        if (configMap.contains(generalDebugModeKey) && configMap[generalDebugModeKey]!!.toBoolean()) {
+            setLogLevel(LogLevel.DEBUG)
+            return
+        }
+        setLogLevel(LogLevel.INFO)
+    }
+
+    private fun initDebugMode() {
+        val configurations = appConfigurationRepository.getConfigurations().configMap
+        handleDebugMode(configurations)
+    }
+
+    private fun setLogLevel(logLevel: LogLevel) {
         val system: LoggingSystem = LoggingSystem.get(this::class.java.getClassLoader())
         system.setLogLevel("org.freekode.tp2intervals", logLevel)
         log.error("Log level set to $logLevel")
-    }
-
-    private fun initLogLevel() {
-        val logLevel = appConfigurationRepository.getConfiguration("generic.log-level")
-        setLogLevel(LogLevel.valueOf(logLevel!!))
     }
 }
