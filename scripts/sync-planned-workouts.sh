9a1a01b672d8abe39ec720d297571a8545b06700
Evgeny <iam@freekode.org>
2025-01-12 17:30:45 +0100

    sync script

--- scripts/sync-planned-workouts.sh
+++ scripts/sync-planned-workouts.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# How to run
+# ./sync-planned-workouts.sh (tomorrow|2025-01-01) (INTERVALS|TRAINING_PEAKS|TRAINER_ROAD) (INTERVALS|TRAINING_PEAKS|TRAINER_ROAD) [standalone]
+#
+# Example, sync planned workouts from TrainerRoad to TrainingPeaks for tomorrow when running standalone app
+# ./sync-planned-workouts.sh tomorrow TRAINER_ROAD TRAINING_PEAKS standalone
+#
+# Example, sync planned workouts from Intervals to TrainingPeaks for January 5th when running in docker or JAR
+# ./sync-planned-workouts.sh 2025-01-05 INTERVALS TRAINING_PEAKS
+
 syncDate=
-sourcePlatform=$2
-targetPlatform=$3
-port=8080
+sourcePlatform=$2 # INTERVALS, TRAINING_PEAKS, TRAINER_ROAD
+targetPlatform=$3 # INTERVALS, TRAINING_PEAKS, TRAINER_ROAD
+port=8080 #
 
 if [ "$1" = "tomorrow" ]; then
     syncDate=$(date -d @$(( $(date +%s) + 86400 )) +%F)
@@ -11,11 +20,16 @@ else
     syncDate=$(date +%F)
 fi
 
-if [ "$1" = "electron" ]; then
+if [ "$4" = "standalone" ]; then
     port=44864
 fi
 
-wget -O - \
-  --header="Content-Type: application/json" \
-  --post-data='{"startDate":"'$syncDate'","endDate":"'$syncDate'","types":["BIKE","VIRTUAL_BIKE","MTB","RUN"],"skipSynced":true,"sourcePlatform":"'$sourcePlatform'","targetPlatform":"'targetPlatform'"}' \
-  http://localhost:$port/api/workout/copy-calendar-to-calendar
+postData='{"startDate":"'$syncDate'","endDate":"'$syncDate'","types":["BIKE","VIRTUAL_BIKE","MTB","RUN"],"skipSynced":true,"sourcePlatform":"'$sourcePlatform'","targetPlatform":"'targetPlatform'"}'
+
+echo $postData
+echo $port
+
+#wget -O - \
+#  --header="Content-Type: application/json" \
+#  --post-data='{"startDate":"'$syncDate'","endDate":"'$syncDate'","types":["BIKE","VIRTUAL_BIKE","MTB","RUN"],"skipSynced":true,"sourcePlatform":"'$sourcePlatform'","targetPlatform":"'targetPlatform'"}' \
+#  http://localhost:$port/api/workout/copy-calendar-to-calendar
