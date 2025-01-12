7a8cfd36ac8198c1643b31d2ba01dd19a324c4a0
Evgeny <940893+freekode@users.noreply.github.com>
2025-01-12 17:57:56 +0100

    Update sync-planned-workouts.sh

--- scripts/sync-planned-workouts.sh
+++ scripts/sync-planned-workouts.sh
@@ -9,15 +9,27 @@
 # Example, sync planned workouts from Intervals to TrainingPeaks for January 5th when running in docker or JAR
 # ./sync-planned-workouts.sh 2025-01-05 INTERVALS TRAINING_PEAKS
 
+
 syncDate=
 sourcePlatform=$2 # INTERVALS, TRAINING_PEAKS, TRAINER_ROAD
 targetPlatform=$3 # INTERVALS, TRAINING_PEAKS, TRAINER_ROAD
-port=8080 #
+port=8080 # 8080, 44864
+
+
+tomorrow_date() {
+    platform=$(uname)
+    if [ "$platform" == "Darwin" ]; then
+        echo $(date -v+1d +"%Y-%m-%d")
+    elif [ "$platform" == "Linux" ]; then
+        echo $(date -d @$(( $(date +%s) + 86400 )) +%F)
+    fi
+    return 0
+}
 
 if [ "$1" = "tomorrow" ]; then
-    syncDate=$(date -d @$(( $(date +%s) + 86400 )) +%F)
+    syncDate=$(tomorrow_date)
 else
-    syncDate=$(date +%F)
+    syncDate=$1
 fi
 
 if [ "$4" = "standalone" ]; then
@@ -26,10 +38,7 @@ fi
 
 postData='{"startDate":"'$syncDate'","endDate":"'$syncDate'","types":["BIKE","VIRTUAL_BIKE","MTB","RUN"],"skipSynced":true,"sourcePlatform":"'$sourcePlatform'","targetPlatform":"'targetPlatform'"}'
 
-echo $postData
-echo $port
-
-#wget -O - \
-#  --header="Content-Type: application/json" \
-#  --post-data='{"startDate":"'$syncDate'","endDate":"'$syncDate'","types":["BIKE","VIRTUAL_BIKE","MTB","RUN"],"skipSynced":true,"sourcePlatform":"'$sourcePlatform'","targetPlatform":"'targetPlatform'"}' \
-#  http://localhost:$port/api/workout/copy-calendar-to-calendar
+wget -O - \
+  --header="Content-Type: application/json" \
+  --post-data=$postData \
+  http://localhost:$port/api/workout/copy-calendar-to-calendar
